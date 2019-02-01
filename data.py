3822e11fe4ca39e098502d2948e3173e2cdaa34e
Wen1163204547 <qw_iiii@163.com>
2019-02-01 15:08:11 +0800

    SHA update test

--- data.py
+++ data.py
@@ -29,8 +29,8 @@ class DataLoader2d(Dataset):
             ct = np.array(ct)
             ct_copy = np.copy(ct)
             print ct.shape
-            ct[2, 1:] = ct_copy[1, 0:-1]
-            ct[0, 0:-1] = ct_copy[1, 1:]
+            ct[0, 1:] = ct_copy[1, 0:-1]
+            ct[2, 0:-1] = ct_copy[1, 1:]
             ct = ct.transpose(1,0,2,3)
             del ct_copy
             return torch.FloatTensor(ct), torch.LongTensor(seg), self.ct_path[index]
